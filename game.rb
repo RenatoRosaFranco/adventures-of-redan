f5a2e3eb88f0462e28c77a6b4f496fd9d911c0a1
Renato Franco <renato_ny@live.com>
2024-03-18 12:06:28 -0300

    feature: adding game screen concept and remove prefixed score.

--- game.rb
+++ game.rb
@@ -27,8 +27,8 @@ class GameWindow < Gosu::Window
     @enemy_spawn_timer = 0
     @paused = false
     @font = Gosu::Font.new(20)
-    @state = :playing
-    @score = 480
+    @state = :title
+    @score = 0
     @boss = nil
     @boss_spawn_threshold = 500
     @explosion_sound = Gosu::Sample.new("assets/sounds/effects/explosion.mp3")
@@ -97,26 +97,45 @@ class GameWindow < Gosu::Window
   end
 
   def draw
+    case @state
+    when :title
+      draw_title_screen
+    when :playing
+      draw_playing_screen
+    when :game_over
+      draw_game_over_screen
+    end
+  end
+
+  def draw_playing_screen
     if @paused
       draw_paused_screen
-    elsif game_over?
-      draw_game_over_screen
     else
       @player.draw
       @enemies.each(&:draw)
       @projectiles.each(&:draw)
       @font.draw_text("HP: #{@player.hp}", 10, self.height - 30, ZOrder::UI, 1.0, 1.0, Gosu::Color::WHITE)
-      
+  
       if @boss
         @boss&.draw
         @boss.draw_projectiles
         draw_boss_hp
       end
-      
+  
       score_text = "Score: #{@score}"
       text_width = @font.text_width(score_text)
       @font.draw_text(score_text, self.width - text_width - 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::WHITE)
     end
+  end
+
+  def draw_title_screen
+    @font.draw_text("Adventures of Redan", 200, 150, ZOrder::UI, 1.0, 1.0, Gosu::Color::WHITE)
+    @font.draw_text("Press 'Enter' to Start", 220, 300, ZOrder::UI, 1.0, 1.0, Gosu::Color::WHITE)
+    @font.draw_text("Press 'Esc' to Quit", 220, 350, ZOrder::UI, 1.0, 1.0, Gosu::Color::WHITE)
+  end
+
+  def start_new_game
+    @state = :playing
   end
 
   def draw_game_over_screen
@@ -147,18 +166,37 @@ class GameWindow < Gosu::Window
   end
 
   def button_down(id)
-    case id
-    when Gosu::KB_SPACE
-      unless @paused
-        projectile_width = 10
-        @projectiles.push(Projectile.new(self, @player.x + @player.width / 2 - projectile_width / 2, @player.y))
+    case @state
+    when :title
+      if id == Gosu::KB_DOWN
+        # Logic to highlight "Quit" (this part will require additional implementation for highlighting)
+      elsif id == Gosu::KB_UP
+        # Logic to highlight "New Game" (similarly, this requires additional logic for highlighting)
+      elsif id == Gosu::KB_RETURN || id == Gosu::KB_ENTER
+        start_new_game
+      elsif id == Gosu::KB_ESCAPE
+        close
       end
-    when Gosu::KB_R
-      restart_game if game_over?
-    when Gosu::KB_Q
-      close if game_over?
-    when Gosu::KB_ESCAPE
-      @paused = !@paused
+    when :playing
+      case id
+      when Gosu::KB_SPACE
+        unless @paused
+          projectile_width = 10
+          @projectiles.push(Projectile.new(self, @player.x + @player.width / 2 - projectile_width / 2, @player.y))
+        end
+      when Gosu::KB_R
+        restart_game if game_over?
+      when Gosu::KB_Q
+        close if game_over?
+      when Gosu::KB_ESCAPE
+        @paused = !@paused
+      end
+    when :game_over
+      if id == Gosu::KB_R
+        restart_game
+      elsif id == Gosu::KB_Q
+        close
+      end
     end
   end
 
